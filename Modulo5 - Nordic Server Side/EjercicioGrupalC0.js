30deeda2f3889b692565503d7a67518f918a10f9
DanaePostai <daniela.postai@mercadolibre.com>
2021-09-10 16:10:34 -0300

    Agrega nuevos ejercicios 10/09

--- Modulo5 - Nordic Server Side/EjercicioGrupalC0.js
+++ Modulo5 - Nordic Server Side/EjercicioGrupalC0.js
@@ -31,7 +31,7 @@ console.log(computadora);
 let compuConGPU=[];
 
 computadora.forEach(element => {
-    
+
     if(element.gpu){
         compuConGPU.push(element)
     }
@@ -55,3 +55,43 @@ console.log('///////////////////////////////');
 console.log('Costo del Viaje: ' + calculoCosto(98.00, 500));
 console.log('///////////////////////////////');
 console.log('///////////////////////////////');
+
+//Crear un array que contenga números del 1 al 100. Retornar un nuevo array que contenga todos los números multiplicados por dos
+
+let numeros = [];
+
+for (let index = 1; index <= 100; index++) {
+    numeros[index] = index;
+
+}
+
+let multiplos2 = numeros.filter(numero => ((numero % 2) == 0))
+
+console.log('Multiplos de 2');
+console.log(multiplos2);
+
+//Del array que devuelve el ejercicio número 5, filtrar los que sean mayores a 5
+
+let multiplosMayor5 = multiplos2.filter(numero => (numero > 5));
+
+console.log('Mayores a 5');
+console.log(multiplosMayor5);
+
+//Del array que devuelve el ejercicio 6, buscar el primero que sea mayor a 10
+console.log('Primero Mayor a 10');
+console.log(multiplosMayor5.find(numero => numero > 10));
+
+//Dado el siguiente array filtremos a los pokemones con poder menor a 10.
+
+let pokemones = [
+    { nombre: 'pikachu', poder: 12 },
+    { nombre: 'bulbasaur', poder: 6 },
+    { nombre: 'charizard', poder: 19 },
+    { nombre: 'squirtle', poder: 3 },
+    { nombre: 'metwo', poder: 6 },
+]
+
+let debiles = pokemones.filter(pokemon => (pokemon.poder < 10))
+
+console.log('Pokemones Debiles');
+console.log(debiles);
